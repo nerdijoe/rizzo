3cc1e83634dd584c3f8bf9eb56b02fcaefc2828a
Bartek <barfek@gmail.com>
2015-10-17 14:52:06 +0200

    Update timeago_spec.js

--- spec/javascripts/lib/core/timeago_spec.js
+++ spec/javascripts/lib/core/timeago_spec.js
@@ -1,31 +1,31 @@
-define([ "jquery", "lib/core/timeago" ], function($, TimeAgo) {
+define([ "jquery", "lib/core/timeago" ], function($, Timeago) {
 
   "use strict";
 
   describe("TimeAgo", function() {
 
-    var timeago;
+    var instance, date;
 
     beforeEach(function() {
-      var date = new Date(new Date() - 1000 * 60 * 60).toISOString(); // 1h ago
+      date = new Date(new Date() - 1000 * 60 * 60).toISOString(); // 1h ago
 
       loadFixtures("timeago.html");
 
       $(".js-timeago").attr("datetime", date);
       $(".js-timeago-full").attr("datetime", date);
 
-      timeago = new TimeAgo();
+      instance = new Timeago();
     });
 
     describe("Initialisation", function() {
 
       it("is defined", function() {
-        expect(timeago).toBeDefined();
+        expect(instance).toBeDefined();
       });
 
       it("finds all elements", function() {
-        expect(timeago.$fullTimeagos).toExist();
-        expect(timeago.$responsiveTimeagos).toExist();
+        expect(instance.els.$fulls).toExist();
+        expect(instance.els.$responsives).toExist();
       });
     });
 
@@ -34,29 +34,29 @@ define([ "jquery", "lib/core/timeago" ], function($, TimeAgo) {
       describe("Screen width > breakpoint", function() {
 
         beforeEach(function() {
-          spyOn(timeago, "_isAboveBreakpoint").and.returnValue(true);
-          timeago.updateAll();
+          spyOn(instance, "_isMobile").and.returnValue(false);
+          instance.update();
         });
 
-        it("displays full strings in all timeagos", function() {
-          expect($(".js-timeago")).toHaveText("an hour ago");
-          expect($(".js-timeago-full")).toHaveText("an hour ago");
+        it("binds full strings to all elements", function() {
+          expect(instance.els.$responsives).toHaveText("an hour ago");
+          expect(instance.els.$fulls).toHaveText("an hour ago");
         });
       });
 
       describe("Screen width < breakpoint", function() {
 
         beforeEach(function() {
-          spyOn(timeago, "_isAboveBreakpoint").and.returnValue(false);
-          timeago.updateAll();
+          spyOn(instance, "_isMobile").and.returnValue(true);
+          instance.update();
         });
 
-        it("displays full string for '.js-timeago-full'", function() {
-          expect($(".js-timeago-full")).toHaveText("an hour ago");
+        it("binds full strings to $fulls only", function() {
+          expect(instance.els.$fulls).toHaveText("an hour ago");
         });
 
-        it("displays short strings for '.js-timeago'", function() {
-          expect($(".js-timeago")).toHaveText("1h");
+        it("binds short strings to $responsives", function() {
+          expect(instance.els.$responsives).toHaveText("1h");
         });
 
         describe("for dates older than 1 month", function() {
@@ -66,12 +66,12 @@ define([ "jquery", "lib/core/timeago" ], function($, TimeAgo) {
           });
 
           it("returns correct month name", function() {
-            var monthName = timeago._getMonthName(null, 3024000000); // 35 days
+            var monthName = instance._getMonthName(null, 3024000000); // 35 days
             expect(monthName).toEqual("Jan");
           });
 
           it("returns correct year", function() {
-            var fullYear = timeago._getFullYear(null, 31968000000); // 370 days
+            var fullYear = instance._getFullYear(null, 31968000000); // 370 days
             expect(fullYear).toEqual("2014");
           });
 
