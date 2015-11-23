f3ba0e5a1495c66188d4628f5162d462825b7c37
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2015-11-23 16:52:56 -0600

    Add analytics events

--- app/helpers/beta_helper.rb
+++ app/helpers/beta_helper.rb
@@ -11,7 +11,7 @@ module BetaHelper
   # Show banner when ?beta=destinations-next is in the URL
   def show_beta_banner
     return 1.0 if params[:beta] == 'destinations-next'
-    
+
     prng = Random.new(Time.now.to_i)
     prng.rand < 0.01
   end
