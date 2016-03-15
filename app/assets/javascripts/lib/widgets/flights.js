91a361c32ed0f3cf0eb1bf084f666fe53b5d6c56
mateusz_kmiecinski <m.kmiecinski@pilot.co>
2016-03-15 18:19:14 +0100

    [#1494] lint style fixes

--- app/assets/javascripts/lib/widgets/flights.js
+++ app/assets/javascripts/lib/widgets/flights.js
@@ -158,12 +158,12 @@ define([
   };
 
   FlightsWidget.prototype._formatDate = function(date) {
-    var month = '' + (date.getMonth() + 1),
-        day = '' + date.getDate(),
+    var month = "" + (date.getMonth() + 1),
+        day = "" + date.getDate(),
         year = date.getFullYear();
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
-    return [year, month, day].join('-');
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+    return [ year, month, day ].join("-");
   };
 
   return FlightsWidget;
